0493df61a8fe1408d0f204c0651c455da6e3fd91
s-andrew <anso2393@yandex.ru>
2018-01-21 21:28:56 +0300

    Added spring security

---
+++ src/main/java/com/tutorial/tutorial/Services/UserDetailsServiceUtil.java
@@ -0,0 +1,32 @@
+package com.tutorial.tutorial.Services;
+
+import com.tutorial.tutorial.Entities.User;
+import com.tutorial.tutorial.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceUtil implements org.springframework.security.core.userdetails.UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDetails loadedUser;
+
+        try {
+            User client = userRepository.findByUsername(username);
+            loadedUser = new org.springframework.security.core.userdetails.User(
+                    client.getUsername(),
+                    client.getPassword(),
+                    client.getRoles());
+        } catch (Exception repositoryProblem){
+            throw new InternalAuthenticationServiceException(repositoryProblem.getMessage(), repositoryProblem);
+        }
+        return  loadedUser;
+    }
+}
